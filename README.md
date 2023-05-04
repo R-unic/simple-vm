893d36b5f420746287dbd7220e3d7c9dd1c4670b
R-unic <roleyspersonal@gmail.com>
2023-05-03 20:58:59 -0700

    docs: add fibonacci example

--- README.md
+++ README.md
@@ -54,3 +54,47 @@ vm = VM.new [ # a = "something" (define do_something) do_something("some value")
 
 vm.run # => something some value
 ```
+
+fibonacci sequence
+```rb
+fib = VM.new [
+  Op::LOAD, 0,
+  Op::PUSH, 1,
+  Op::LT,
+
+  Op::JZ, 10, # if false jump to 10 (noop)
+  Op::LOAD, 0, # n
+  Op::RETURN,
+
+  Op::NOOP, # else (this is unnecessary, just for readability)
+  Op::LOAD, 0, # n
+  Op::PUSH, 2, # 1
+  Op::SUB, # n - 1
+  Op::CALL, 3, 1, # fib(n - 1)
+
+  Op::LOAD, 0, # n
+  Op::PUSH, 1, # 2
+  Op::SUB, # n - 2
+  Op::CALL, 3, 1, # fib(n - 2)
+  Op::ADD, # fib(n - 1) + fib(n - 2)
+
+  Op::RETURN
+], [
+  "n", 2_i64, 1_i64, "fib"
+] of Types::ValidType
+
+vm = VM.new [
+  Op::PUSH, 2,
+  Op::PUSH, 0,
+  Op::PROC, 1, 1,
+
+  Op::PUSH, 3,
+  Op::CALL, 1, 1,
+  Op::ECHO,
+  Op::END
+], [
+  fib, "fib", "n", 25_i64
+] of Types::ValidType
+
+vm.run # 75025
+```
--- src/test.cr
+++ src/test.cr
@@ -3,45 +3,16 @@ require "./opcodes"
 require "./types"
 require "benchmark"
 
-fib = VM.new [
-  Op::LOAD, 0,
-  Op::PUSH, 1,
-  Op::LT,
-
-  Op::JZ, 10, # if false jump to 10 (noop)
-  Op::LOAD, 0, # n
-  Op::RETURN,
-
-  Op::NOOP, # else (this is unnecessary, just for readability)
-  Op::LOAD, 0, # n
-  Op::PUSH, 2, # 1
-  Op::SUB, # n - 1
-  Op::CALL, 3, 1, # fib(n - 1)
-
-  Op::LOAD, 0, # n
-  Op::PUSH, 1, # 2
-  Op::SUB, # n - 2
-  Op::CALL, 3, 1, # fib(n - 2)
-  Op::ADD, # fib(n - 1) + fib(n - 2)
-
-  Op::RETURN
-], [
-  "n", 2_i64, 1_i64, "fib"
-] of Types::ValidType
-
 vm = VM.new [
-  Op::PUSH, 2,
   Op::PUSH, 0,
-  Op::PROC, 1, 1,
-
-  Op::PUSH, 3,
-  Op::CALL, 1, 1,
+  Op::PUSH, 1,
+  Op::CONCAT,
   Op::ECHO,
   Op::END
 ], [
-  fib, "fib", "n", 25_i64
+  "hello ", "world"
 ] of Types::ValidType
 
 Benchmark.bm do |x|
-  x.report "fib 25 = " { vm.run }
+  x.report "msg = " { vm.run }
 end
